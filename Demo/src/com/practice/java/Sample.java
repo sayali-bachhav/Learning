c46cb165bd5cc3f6edf93637cd7f0109e13cc665
sayali-bachhav <sayalibachhavcet@gmail.com>
2022-12-04 22:10:08 +0530

    third commit

--- Demo/src/com/practice/java/Sample.java
+++ Demo/src/com/practice/java/Sample.java
@@ -1,6 +1,9 @@
 package com.practice.java;
 
 public class Sample {
+	public void read() {
+		System.out.println("read method");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("hello");
