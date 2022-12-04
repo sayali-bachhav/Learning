15772922d85cf30c2454bc2ef3a20f887fd6e228
sayali-bachhav <sayalibachhavcet@gmail.com>
2022-12-04 21:56:19 +0530

    fourth commit

--- Demo/src/com/practice/java/Sample.java
+++ Demo/src/com/practice/java/Sample.java
@@ -5,11 +5,16 @@ public class Sample {
 		System.out.println("this is add method");
 	}
 
+	public void read() {
+		System.out.println("this is read method");
+	}
+
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("hi sayali");
 		Sample s = new Sample();
 		s.add();
+		s.read();
 	}
 
 }
